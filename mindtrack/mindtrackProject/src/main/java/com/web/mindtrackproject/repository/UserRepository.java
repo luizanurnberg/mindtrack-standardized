87738e4a8b68ec4b628fdbfb697cc367ad4856d5
Luíza Nurnberg <luiza.nurnberg@allog.com.br>
2023-12-15 21:32:25 -0300

    feat(factory method): add factory method to UserRepository

--- mindtrack/mindtrackProject/src/main/java/com/web/mindtrackproject/repository/UserRepository.java
+++ mindtrack/mindtrackProject/src/main/java/com/web/mindtrackproject/repository/UserRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    static UserRepository create() {
+        return new UserRepositoryImpl();
+    }
+
     User findByEmail(String email);
 }
