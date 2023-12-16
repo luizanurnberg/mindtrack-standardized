481a1caa74078c7c52754d714710bf55338d6522
luizanurnberg <nurnbergluiza@gmail.com>
2023-12-16 14:53:58 -0300

    feat(observer): started observer implementation

--- mindtrack/mindtrackProject/src/main/java/com/web/mindtrackproject/service/NoteService.java
+++ mindtrack/mindtrackProject/src/main/java/com/web/mindtrackproject/service/NoteService.java
@@ -6,15 +6,20 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.web.mindtrackproject.command.DeleteNoteCommand;
 import com.web.mindtrackproject.command.CommandInvoker;
+import com.web.mindtrackproject.service.observer.Subject;
+import com.web.mindtrackproject.service.observer.observer;
+
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-public class NoteService {
+public class NoteService implements Subject{
     private final NoteRepository noteRepository;
     private final CommandInvoker commandInvoker;
+    private List<Observer> observers = new ArrayList<>();
+
 
     public Note createNote(Note note) {
         return noteRepository.save(note);
@@ -67,5 +72,23 @@ public class NoteService {
     public List<Note> getNotesByContent(String content) {
         return noteRepository.getNotesByContent(content);
     }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
 
 }
--- mindtrack/mindtrackProject/src/main/java/com/web/mindtrackproject/service/UserService.java
+++ mindtrack/mindtrackProject/src/main/java/com/web/mindtrackproject/service/UserService.java
@@ -5,15 +5,17 @@ import com.web.mindtrackproject.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import com.web.mindtrackproject.service.NoteService;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-public class UserService {
+public class UserService implements Observer{
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final NoteService noteService;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -27,6 +29,8 @@ public class UserService {
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encryptedPassword);
         User savedUser = userRepository.save(user);
+        // Após criar o usuário, registre o UserService como observador
+        noteService.addObserver(savedUser);
         return savedUser;
     }
 
@@ -42,5 +46,11 @@ public class UserService {
         } else {
             throw new Exception("Email e/ou senha inválidos.");
         }
+    }
+
+    @Override
+    public void update() {
+        //TODO
+        // Lógica para reagir a mudanças nas notas
     }
 }
